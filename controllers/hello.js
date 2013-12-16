c32f3f97492dbd6c235d2d4f6cad96c0cfebc2ce
ravenjohn <rjlagrimas08@gmail.com>
2013-12-16 20:03:42 +0800

    Added locked fields

--- controllers/hello.js
+++ controllers/hello.js
@@ -4,6 +4,7 @@ r.index_get = function () {
     return {message : "hell1"};
 }
 r.index_get.rqd_flds = ['name', 'sex', 'contact_number', 'birthday', 'age'];
+r.index_get.lckd_flds = ['asdf'];
 
 
 r.r_get = function () {
